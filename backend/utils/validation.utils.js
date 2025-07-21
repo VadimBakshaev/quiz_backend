88252d7e65edd7134c28903245d344b05a987855
Vadim <vadimm@citydom.ru>
2025-07-21 14:23:38 +0300

    add signup

--- backend/utils/validation.utils.js
+++ backend/utils/validation.utils.js
@@ -5,7 +5,7 @@ class ValidationUtils {
         const schema = Joi.object({
             name: Joi.string().min(3).required(),
             lastName: Joi.string().min(3).required(),
-            email: Joi.string().min(6).required().email(),
+            email: Joi.string().min(3).required().email(),
             password: Joi.string().min(6).required(),
         });
         return schema.validate(data);
@@ -13,7 +13,7 @@ class ValidationUtils {
 
     static loginValidation (data) {
         const schema = Joi.object({
-            email: Joi.string().min(6).required().email(),
+            email: Joi.string().min(3).required().email(),
             password: Joi.string().min(6).required(),
         });
         return schema.validate(data);
--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -1,38 +1,49 @@
 export class Form {
-    constructor() {
+    constructor(page) {
         this.formElement = null;
         this.agreeElement = null;
         this.processElement = null;
+        this.page = page;
+        const that = this;
         this.fields = [
-            {
-                name: 'firstName',
-                element: null,
-                regex: /^[А-Я][а-я]+\s*$/,
-                valid: false
-            },
-            {
-                name: 'lastName',
-                element: null,
-                regex: /^[А-Я][а-я]+\s*$/,
-                valid: false
-            },
             {
                 name: 'email',
                 element: null,
                 regex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                 valid: false
+            },
+            {
+                name: 'password',
+                element: null,
+                regex: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+                valid: false
             }
         ];
-        const that = this;
+        if (this.page === 'signup') {
+            this.fields.unshift(
+                {
+                    name: 'firstName',
+                    element: null,
+                    regex: /^[А-Я][а-я]+\s*$/,
+                    valid: false
+                },
+                {
+                    name: 'lastName',
+                    element: null,
+                    regex: /^[А-Я][а-я]+\s*$/,
+                    valid: false
+                }
+            );
+            this.agreeElement = document.getElementById('agree');
+            this.agreeElement.addEventListener('change', function () {
+                that.validateForm();
+            });
+        };
         this.fields.forEach(item => {
             item.element = document.getElementById(item.name);
             item.element.addEventListener('change', function () {
                 that.validateField(item, this);
             })
-        });
-        this.agreeElement = document.getElementById('agree');
-        this.agreeElement.addEventListener('change', function () {
-            that.validateForm();
         });
         this.formElement = document.querySelector('.form');
         this.formElement.addEventListener('submit', function (e) {
@@ -52,7 +63,12 @@ export class Form {
         this.validateForm();
     };
     validateForm() {
-        if (this.fields.every(item => item.valid) && this.agreeElement.checked) {
+        if (
+            this.agreeElement
+                ? this.fields.every(item => item.valid)
+                && this.agreeElement.checked
+                : this.fields.every(item => item.valid)
+        ) {
             this.processElement.removeAttribute('disabled');
             return true;
         } else {
@@ -60,15 +76,46 @@ export class Form {
             return false;
         };
     };
-    processForm() {
+    async processForm() {
         if (this.validateForm()) {
-            const user = {};
-            this.fields.forEach(item => {
-                user[item.name] = item.element.value;
-            });
+            if (this.page === 'signup') {
+                try {
+                    const response = await fetch('http://localhost:3000/api/signup', {
+                        method: "POST",
+                        headers: {
+                            'Content-type': 'application/json',
+                            'Accept': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            'name': this.fields.find(item => item.name === 'firstName').element.value,
+                            'lastName': this.fields.find(item => item.name === 'lastName').element.value,
+                            'email': this.fields.find(item => item.name === 'email').element.value,
+                            'password': this.fields.find(item => item.name === 'password').element.value
+                        })
+                    });
+                    const result = await response.json();
+                    if (response.status < 200 || response.status >= 300) {
+                        throw new Error(response.message);
+                    };
+                    if (result) {
+                        if (result.error || !result.user) {
+                            throw new Error(result.message);
+                        };
+                        location.href = '#/choice';
+                    };
+                } catch (error) {
+                    console.log(error);
+                };
+            } else {
 
-            sessionStorage.setItem('user', JSON.stringify(user));
-            location.href = '#/choice';
+            };
+            // const user = {};
+            // this.fields.forEach(item => {
+            //     user[item.name] = item.element.value;
+            // });
+
+            // sessionStorage.setItem('user', JSON.stringify(user));
+            // location.href = '#/choice';
         };
     };
 };
