c26cf75d95a9c8b923c8b7360148355fab64b4e7
Vadim <vadimm@citydom.ru>
2025-07-26 00:10:28 +0300

    restore webpack.config.js

---
+++ frontend/webpack.config.js
@@ -0,0 +1,25 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "templates", to: "templates" },
+                { from: "styles", to: "styles" },
+                { from: "static/fonts", to: "fonts" },
+                { from: "static/images", to: "images" },
+            ],
+        }),
+    ],
+};
